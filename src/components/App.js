9b77ebe7651552101f21058bf561c19ea500654e
Madhab Paul <madhab12paul@gmail.com>
2021-01-09 19:49:12 +0530

    Material UI added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,72 @@
 import React from "react";
 import './style/App.css';
 import Weather from "./Weather";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import WbSunnyTwoToneIcon from '@material-ui/icons/WbSunnyTwoTone';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        WeatherTrac
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: "center"
+  },
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+  },
+}));
 
 function App() {
+
+  const classes = useStyles();
+
   return (
+    <>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}><WbSunnyTwoToneIcon fontSize="small" style={{ color: 'orange' }} />
+            WeatherTrac
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
    <Weather />
+   <div className={classes.root}>      
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Typography variant="body1">Made with ❤️ | Developer 👉 <Link href="https://github.com/madhabpaul" >Madhab Paul </Link></Typography>
+          <Copyright />
+        </Container>
+      </footer>
+    </div>
+   </>
   );
 }
 
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,7 +1,51 @@
 import React, {useState, useEffect} from 'react';
+import  "./style/Weather.css";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import DnsIcon from '@material-ui/icons/Dns';
+import QueryBuilderIcon from '@material-ui/icons/QueryBuilder';
+import FilledInput from "@material-ui/core/FilledInput";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import LocationCityIcon from '@material-ui/icons/LocationCity';
+import Grid from '@material-ui/core/Grid';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      minHeight: '100vh',
+    },
+    main: {
+      marginTop: theme.spacing(8),
+      marginBottom: theme.spacing(2),
+    },
+    footer: {
+      padding: theme.spacing(3, 2),
+      marginTop: 'auto',
+      backgroundColor:
+        theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+    },
+    margin: {
+        margin: theme.spacing(1)
+      },
+      withoutLabel: {
+        marginTop: theme.spacing(3)
+      },
+      textField: {
+        width: "25ch"
+      }
+  }));
+
 
 function Weather() {
-
+    const classes = useStyles();
     
     const [ip, setip] = useState("");
     const [ipName, setipName] = useState("");
@@ -22,17 +66,10 @@ function Weather() {
         //     var lat = position.coords.latitude;
         //     var lon = position.coords.longitude;
 
-
-            const fetchIp = async () => {
-                const urlIp = "https://api.ipify.org?format=json";
-                const resIp = await fetch(urlIp);
-                const ipJson = await resIp.json();
-                // console.log(ipJson.ip);
-                var ipAddress = ipJson.ip;
             
             
                 const fetchCity = async () => {
-                    const urlCity = "https://ipapi.co/"+ ipAddress +"/json/";
+                    const urlCity = "https://freegeoip.app/json/";
                     const result = await fetch(urlCity);
                     const cityJson = await result.json();
                     // console.log(cityJson);
@@ -40,12 +77,12 @@ function Weather() {
                     setSearch(cityJson.city);
                 };
                     fetchCity();   
-            };
-                fetchIp();
+            
+               
                     
         // }
         // getLocation();
-
+        //https://ipapi.co/"+ ipAddress +"/json/
     },[] )
     
     
@@ -67,28 +104,123 @@ function Weather() {
 
     return (
         <>
-           
-            <p><b>Your current location is</b> {ip.city}, {ip.region}, {ip.country_name}. <b>Timezone</b> {ip.timezone}. <b>Your IP Address </b>{ip.ip}</p>
-            
-            <div>
-           
-                <input type="search" onChange={(event) => {
+           <CssBaseline />
+            <Container component="main" className={classes.main} maxWidth="sm">
+            <div style={{ width: '100%' }}>
+                <Box component="div" display="inline" p={1} m={1} >
+                    <LocationOnIcon fontSize="small" style={{ color: 'green' }}/> {ip.city}, {ip.region_name}, {ip.country_name} 
+                </Box>
+                <Box component="div" display="inline" p={1} m={1} >
+                    <QueryBuilderIcon fontSize="small" style={{ color: 'blue' }}/> {ip.time_zone}
+                </Box>
+                <Box component="div" display="inline" p={1} m={1} >
+                    <DnsIcon fontSize="small" style={{ color: 'red' }}/> {ip.ip}
+                </Box>
+            </div>
+            <div className={classes.root}>
+            <FormControl fullWidth className={classes.margin} variant="filled">
+                <InputLabel htmlFor="filled-adornment-amount">Search City</InputLabel>
+                <FilledInput
+                    id="filled-adornment-amount"
+                    
+                    onChange={(event) => {
                     setSearch(event.target.value)
-                }} placeholder="Search City"/>
-                
-            </div>
-         {!city || !ipName ? (
-             <p>No city found</p>
-         ) : (
-             
-            <div>
-                <h2>{ipName.name}</h2>
-                <p><b>Current Temperature </b>{city.temp_c} Celsius <b>Condition</b> {city.condition.text} <img src={city.condition.icon} alt="icon" /> <b>UV</b> {city.uv}</p>
-            </div>
+                }}
+                    startAdornment={<InputAdornment position="start"><LocationCityIcon fontSize="small" style={{ color: 'purple' }}/></InputAdornment>}
+                />
+            </FormControl>
             
+         {!city || !ipName ? (
+            <Typography variant="h5" component="h2" gutterBottom>
+                No city found
+            </Typography> 
+         ) : (
+             <div>
+            <Typography variant="h3" component="h2" gutterBottom>
+                {ipName.name}, {ipName.country}
+            </Typography>
+                        
+            <Grid container>
+                <Box
+                    boxShadow={3}
+                    bgcolor="background.paper"
+                    m={1}
+                    p={1}
+                    style={{ width: '8rem', height: '5rem' }}
+                >
+                   <b>Temperature</b> <br /> {city.temp_c}&#176; C<br />{city.temp_f}&#176; F
+                </Box>
+                <Box
+                    boxShadow={3}
+                    bgcolor="background.paper"
+                    m={1}
+                    p={1}
+                    style={{ width: '8rem', height: '5rem' }}
+                >
+                   <b>Feels Like</b> <br /> {city.feelslike_c}&#176; C<br />{city.feelslike_f}&#176; F
+                </Box>
+                <Box
+                    boxShadow={3}
+                    bgcolor="background.paper"
+                    m={1}
+                    p={1}
+                    style={{ width: '8rem', height: '5rem' }}
+                >
+                   <b>Humidity</b> <br /> {city.humidity} 
+                </Box>
+                <Box
+                    boxShadow={3}
+                    bgcolor="background.paper"
+                    m={1}
+                    p={1}
+                    style={{ width: '8rem', height: '5rem' }}
+                >
+                   <b>Cloud</b> <br /> {city.cloud}
+                </Box>
+                <Box
+                    boxShadow={3}
+                    bgcolor="background.paper"
+                    m={1}
+                    p={1}
+                    style={{ width: '8rem', height: '5rem' }}
+                >
+                   <b>Wind Direction</b> <br /> {city.wind_degree}&#176; {city.wind_dir}
+                </Box>
+                <Box
+                    boxShadow={3}
+                    bgcolor="background.paper"
+                    m={1}
+                    p={1}
+                    style={{ width: '8rem', height: '5rem' }}
+                >
+                   <b>Wind Speed</b> <br /> {city.wind_kph} Kmph <br />{city.wind_mph} Mph
+                </Box>
+                <Box
+                    boxShadow={3}
+                    bgcolor="background.paper"
+                    m={1}
+                    p={1}
+                    style={{ width: '8rem', height: '5rem' }}
+                >
+                   <b>Visibility</b> <br /> {city.vis_km} Km <br />{city.vis_miles} M
+                </Box>
+                <Box
+                    boxShadow={3}
+                    bgcolor="background.paper"
+                    m={1}
+                    p={1}
+                    style={{ width: '8rem', height: '5rem' }}
+                >
+                   <b>UV</b> <br /> {city.uv}
+                </Box>
+            </Grid>
+            </div>
             )
 
          }
+         </div>
+        </Container>
+            
         </>
     )
 }
